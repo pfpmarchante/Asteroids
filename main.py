27a55d6de918064938b8099417e9d98d9a98f78b
Pedro Marchante <ninjoi@pop-os.home>
2025-04-19 21:27:40 +0100

    Added Player to the background

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import pygame 
 from constants import *
+from player import Player
+from circleshape import CircleShape 
+
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
     
     clock = pygame.time.Clock()
     dt = 0
@@ -13,8 +17,10 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000.0
+        
         
     
    
