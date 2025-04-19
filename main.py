b982a582a6327d90f0bcfce7fe70fa2e64a6056e
ninjoi <pfpmarchante96@gmail.com>
2025-04-19 17:17:50 +0100

    Fixed the refresh rate of the background

--- main.py
+++ main.py
@@ -5,16 +5,19 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     
+    clock = pygame.time.Clock()
+    dt = 0
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
         pygame.display.flip()
+        dt = clock.tick(60) / 1000.0
+        
     
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+   
     
     
     
